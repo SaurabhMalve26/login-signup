835874d00e62e4cb5d32b7748c04a6ac93cec178
Saurabh Malve <malve.saurabh45@gmail.com>
2024-06-11 12:54:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginSignup from './Components/LoginSingup/LoginSignup.jsx';
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
